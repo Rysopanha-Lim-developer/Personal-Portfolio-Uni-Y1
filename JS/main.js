097ca54f4ffc9257220a448fa9ac79e8c5d6d3c3
Rysopanha <sopanhalimry@gmail.com>
2026-06-22 13:02:53 +0700

    finished responsive fro max-width:600px for education section and footer

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,34 @@
-//Nav toggle for small screen
-const navToggler = document.getElementById("nav-toggler")
-const navRetract = document.getElementById("nav-retract")
-const navBar = document.getElementById("navigator")
-const sectionLink = document.querySelectorAll(".section-link")
-const techCard = document.querySelectorAll(".techCard")
-const header = document.getElementById("navigation")
+document.addEventListener("DOMContentLoaded", () => {
+    //Nav toggle for small screen
+    const navToggler = document.getElementById("nav-toggler")
+    const navRetract = document.getElementById("nav-retract")
+    const navBar = document.getElementById("navigator")
+    const sectionLink = document.querySelectorAll(".section-link")
+    const techCard = document.querySelectorAll(".techCard")
+    const header = document.getElementById("navigation")
 
-navToggler.addEventListener("click", () =>{
-    header.classList.add("dropDown")
-    navToggler.classList.add("hide-burger-nav")
-    navRetract.classList.add("show-x-nav")
-    navBar.classList.add("showNav")
-    navBar.removeAttribute('inert');
-})
-navRetract.addEventListener("click", () =>{
-    header.classList.remove("dropDown")
-    navToggler.classList.remove("hide-burger-nav")
-    navRetract.classList.remove("show-x-nav")
-    navBar.classList.remove("showNav")
-    navBar.setAttribute('inert', '');
-})
-techCard.forEach(card => {
-    card.addEventListener("click", () => {
-        
-        const cardAnimation = card.classList.contains("animated") ? card.classList.remove("animated"):card.classList.add("animated")
+    navToggler.addEventListener("click", () =>{
+        header.classList.add("dropDown")
+        navToggler.classList.add("hide-burger-nav")
+        navRetract.classList.add("show-x-nav")
+        navBar.classList.add("showNav")
+        navBar.removeAttribute('inert');
+    })
+    navRetract.addEventListener("click", () =>{
+        header.classList.remove("dropDown")
+        navToggler.classList.remove("hide-burger-nav")
+        navRetract.classList.remove("show-x-nav")
+        navBar.classList.remove("showNav")
+        navBar.setAttribute('inert', '');
+    })
+    techCard.forEach(card => {
+        card.addEventListener("click", () => {
+            
+            const cardAnimation = card.classList.contains("animated") ? card.classList.remove("animated"):card.classList.add("animated")
 
-        setTimeout(()=>{
-            card.classList.remove("animated")
-        }, 3000)
+            setTimeout(()=>{
+                card.classList.remove("animated")
+            }, 3000)
+        })
     })
 })
